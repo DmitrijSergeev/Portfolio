6e30784b344654e6597ce22a2ec16aa6490967cd
Dmitry Sergeev <dmitrijsergeev@gmail.com>
2024-02-13 17:16:30 +0400

    add components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
+import { AboutMe } from '@/components/aboutMe/aboutMe'
+import { Contacts } from '@/components/contacts/contacts'
+import { Footer } from '@/components/footer/footer'
 import { Header } from '@/components/header/header'
+import { Main } from '@/components/main/main'
+import { Projects } from '@/components/projects/propjects'
+import { Skills } from '@/components/skills/skills'
 
 export function App() {
   return (
     <div>
       <Header />
+      <Main />
+      <AboutMe />
+      <Skills />
+      <Projects />
+      <Contacts />
+      <Footer />
     </div>
   )
 }
